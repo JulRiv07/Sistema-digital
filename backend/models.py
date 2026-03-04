1bc3d08c2dc36ea4440fdb1d3ff553722b61e474
JulRiv07 <Julian.rivera@utp.edu.co>
2026-03-04 00:49:58 -0500

    fix internal imports for render

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.sql import func 
-from database import Base
+from backend.database import Base
 
 class Cliente(Base):
     __tablename__ = "clientes"
